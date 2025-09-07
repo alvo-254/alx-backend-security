556968e550ad6a8596bb550032c6d2c865e08a22
Alvin <162141395+alvo-254@users.noreply.github.com>
2025-09-07 08:36:07 +0300

    Create RequestLog model for IP tracking

---
+++ ip_tracking/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+class RequestLog(models.Model):
+    ip_address = models.GenericIPAddressField()
+    timestamp = models.DateTimeField()
+    path = models.CharField(max_length=255)
+
+    country = models.CharField(max_length=100, blank=True, null=True)
+    city = models.CharField(max_length=100, blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.ip_address} at {self.timestamp}"
