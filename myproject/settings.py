107b7514986ed375257ee6ee86287d65bff6bd5a
Alvin <162141395+alvo-254@users.noreply.github.com>
2025-09-07 08:47:24 +0300

    Add IP logging middleware to settings

---
+++ myproject/settings.py
@@ -0,0 +1,13 @@
+MIDDLEWARE = [
+    # default Django middleware...
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+
+    # add IP logging middleware
+    "ip_tracking.middleware.IPLoggingMiddleware",
+]
