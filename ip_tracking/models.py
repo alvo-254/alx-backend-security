227cfdc7dbe7d6926080ca2292b59fc239fe5443
Alvin <162141395+alvo-254@users.noreply.github.com>
2025-09-07 08:39:05 +0300

    Update models.py

--- ip_tracking/models.py
+++ ip_tracking/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class RequestLog(models.Model):
     ip_address = models.GenericIPAddressField()
     timestamp = models.DateTimeField()
@@ -10,3 +11,19 @@ class RequestLog(models.Model):
 
     def __str__(self):
         return f"{self.ip_address} at {self.timestamp}"
+
+
+class BlockedIP(models.Model):
+    ip_address = models.GenericIPAddressField(unique=True)
+
+    def __str__(self):
+        return self.ip_address
+
+
+class SuspiciousIP(models.Model):
+    ip_address = models.GenericIPAddressField()
+    reason = models.TextField()
+    flagged_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.ip_address} - {self.reason}"
